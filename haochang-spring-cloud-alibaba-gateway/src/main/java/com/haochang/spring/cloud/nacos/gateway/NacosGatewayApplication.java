8f60c20c235e1966f5fb00c8c6cb7bc41a199f83
youzhi.gao <youzhi.gao@ikang.com>
2020-03-20 17:43:24 +0800

    整合gateway

---
+++ haochang-spring-cloud-alibaba-gateway/src/main/java/com/haochang/spring/cloud/nacos/gateway/NacosGatewayApplication.java
@@ -0,0 +1,20 @@
+package com.haochang.spring.cloud.nacos.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+/**
+ * @description: 描述：网关启动类
+ * @author: youzhi.gao@ikang.com
+ * @date: 2020-03-20 15:29
+ */
+@SpringBootApplication
+@EnableFeignClients
+@EnableDiscoveryClient
+public class NacosGatewayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(NacosGatewayApplication.class, args);
+    }
+}
