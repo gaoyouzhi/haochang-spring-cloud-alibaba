a919a00b577c7f7a49d62a1706b2619b4a95b088
youzhi.gao <youzhi.gao@ikang.com>
2020-03-24 10:19:35 +0800

    整合rocketmq

--- haochang-spring-cloud-alibaba-nacos-rocketmq-provider/src/main/java/com/haochang/spring/cloud/alibaba/rocketmq/provider/RocketMQProviderApplication.java
+++ haochang-spring-cloud-alibaba-nacos-rocketmq-provider/src/main/java/com/haochang/spring/cloud/alibaba/rocketmq/provider/RocketMQProviderApplication.java
@@ -1,32 +1,32 @@
-//package com.haochang.spring.cloud.alibaba.rocketmq.provider;
-//
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.boot.CommandLineRunner;
-//import org.springframework.boot.SpringApplication;
-//import org.springframework.boot.autoconfigure.SpringBootApplication;
-//import org.springframework.cloud.stream.annotation.EnableBinding;
-//import org.springframework.cloud.stream.messaging.Source;
-//import org.springframework.messaging.MessageChannel;
-//import org.springframework.messaging.support.MessageBuilder;
-//
-///**
-// * @description: 描述：rocketmq 服务者启动类
-// * @author: youzhi.gao@ikang.com
-// * @date: 2020-03-23 09:48
-// */
-//@SpringBootApplication
-//@EnableBinding({Source.class})
-//public class RocketMQProviderApplication implements CommandLineRunner {
-//
-//    @Autowired
-//    private MessageChannel output;
-//
-//    public static void main(String[] args) {
-//        SpringApplication.run(RocketMQProviderApplication.class, args);
-//    }
-//
-//    @Override
-//    public void run(String... args) throws Exception {
-//        output.send(MessageBuilder.withPayload("Hello RocketMQ").build());
-//    }
-//}
+package com.haochang.spring.cloud.alibaba.rocketmq.provider;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.stream.annotation.EnableBinding;
+import org.springframework.cloud.stream.messaging.Source;
+import org.springframework.messaging.MessageChannel;
+import org.springframework.messaging.support.MessageBuilder;
+
+/**
+ * @description: 描述：rocketmq 服务者启动类
+ * @author: youzhi.gao@ikang.com
+ * @date: 2020-03-23 09:48
+ */
+@SpringBootApplication
+@EnableBinding({Source.class})
+public class RocketMQProviderApplication implements CommandLineRunner {
+
+    @Autowired
+    private MessageChannel output;
+
+    public static void main(String[] args) {
+        SpringApplication.run(RocketMQProviderApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        output.send(MessageBuilder.withPayload("Hello RocketMQ").build());
+    }
+}
--- haochang-spring-cloud-alibaba-nacos-rocketmq-provider/src/main/java/com/haochang/spring/cloud/alibaba/rocketmq/provider/test/ProviderSingle.java
+++ haochang-spring-cloud-alibaba-nacos-rocketmq-provider/src/main/java/com/haochang/spring/cloud/alibaba/rocketmq/provider/test/ProviderSingle.java
@@ -21,7 +21,7 @@ public class ProviderSingle {
         producer.start();
         for (int i = 0; i < 10; i++) {
             //Create a message instance, specifying topic, tag and message body.
-            Message msg = new Message("TopicTest" /* Topic */,
+            Message msg = new Message("test-topic" /* Topic */,
                     "TagA" /* Tag */,
                     ("Hello RocketMQ " +
                             i).getBytes(RemotingHelper.DEFAULT_CHARSET) /* Message body */
