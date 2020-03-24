013aeabc94b213eb7c03f2ac3d269503b1430699
youzhi.gao <youzhi.gao@ikang.com>
2020-03-24 09:56:10 +0800

    整合rocketmq

---
+++ haochang-spring-cloud-alibaba-nacos-rocketmq-provider/src/main/java/com/haochang/spring/cloud/alibaba/rocketmq/provider/test/ProviderSingle.java
@@ -0,0 +1,38 @@
+package com.haochang.spring.cloud.alibaba.rocketmq.provider.test;
+
+import org.apache.rocketmq.client.producer.DefaultMQProducer;
+import org.apache.rocketmq.client.producer.SendResult;
+import org.apache.rocketmq.common.message.Message;
+import org.apache.rocketmq.remoting.common.RemotingHelper;
+
+/**
+ * @description: 描述：单机mq测试
+ * @author: youzhi.gao@ikang.com
+ * @date: 2020-03-24 09:42
+ */
+public class ProviderSingle {
+    public static void main(String[] args) throws Exception{
+        //Instantiate with a producer group name.
+        DefaultMQProducer producer = new
+                DefaultMQProducer("please_rename_unique_group_name");
+        // Specify name server addresses.
+        producer.setNamesrvAddr("localhost:9876");
+        //Launch the instance.
+        producer.start();
+        for (int i = 0; i < 10; i++) {
+            //Create a message instance, specifying topic, tag and message body.
+            Message msg = new Message("TopicTest" /* Topic */,
+                    "TagA" /* Tag */,
+                    ("Hello RocketMQ " +
+                            i).getBytes(RemotingHelper.DEFAULT_CHARSET) /* Message body */
+            );
+            //Call send message to deliver message to one of brokers.
+            SendResult sendResult = producer.send(msg);
+            System.out.printf("%s%n", sendResult);
+        }
+        //Shut down once the producer instance is not longer in use.
+        producer.shutdown();
+    }
+
+
+}
